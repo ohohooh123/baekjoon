cb690d231d603101280cb1bfb3c03eb211daa20f
yeon <ohohooh@naver.com>
2023-06-14 23:25:09 +0900

    6웗14완성

--- 14888 연산자 끼워넣기/14888.cpp
+++ 14888 연산자 끼워넣기/14888.cpp
@@ -1,12 +1,20 @@
+
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
+#define PLUS 0
+#define MINUS 1
+#define MUL 2
+#define DIV 3
+
 int main()
 {
     int N, temp;
     int arr[4];
+    int Min = 10000000000, Max = -10000000000;
     vector <int> num;
     vector <int> oper;
     cin>>N;
@@ -22,4 +30,29 @@ int main()
             }
         }
     }
+    
+    do{
+        int sum = num[0];
+        for(int i=0;i<N-1;i++){
+            switch(oper[i]){
+                case PLUS:
+                sum+=num[i+1];
+                break;
+                case MINUS:
+                sum-=num[i+1];
+                break;
+                case MUL:
+                sum*=num[i+1];
+                break;
+                case DIV:
+                sum/=num[i+1];
+                break;
+            }
+        }
+        Min=min(Min,sum);
+        Max=max(Max,sum);
+    }while(next_permutation(oper.begin(),oper.end()));
+    
+    cout<<Max<<"\n"<<Min;
+    
 }
